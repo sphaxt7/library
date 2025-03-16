f68c9f02bb579f982f9d38df5c481fb8c00ed8f0
sphaxt7 <issamtoxico@gmail.com>
2025-03-16 05:41:10 +0100

    uploading from local to remote

---
+++ index.js
@@ -0,0 +1,87 @@
+let myLibrary = [];
+
+function Book(title, author, pages, is_read) {
+	if (!new.target) {
+		throw Error("you must use the 'new' keywoard to create this object");
+  	}
+
+	this.title = `"${title}"`;
+	this.author = author;
+	this.pages = `${pages} pages`;
+	this.is_read = is_read ? "Read" : "Not read";
+}
+
+function addBookToLibrary() {
+	let title = document.querySelector('#title').value;
+	let author = document.querySelector('#author').value;
+	let pages = document.querySelector('#pages').value;
+	let completed = document.querySelector('#completed').checked;
+	
+	const myBook = new Book(title, author, pages, completed);
+	myLibrary.push(myBook);
+	// console.log(...myLibrary);
+	createCard()
+	
+}
+
+function createCard() {
+	bookGrid.innerHTML = ""
+	myLibrary.forEach((myBook, index) => {
+		const bookCard = document.createElement('div');
+		bookCard.classList.add('book-card');
+
+		const title = document.createElement('p');
+		title.textContent = myBook.title;
+		const author = document.createElement('p');
+		author.textContent = myBook.author;
+		const pages = document.createElement('p');
+		pages.textContent = myBook.pages;
+		const is_read = document.createElement('button');
+		is_read.textContent = myBook.is_read;
+		// is_read.id = `is-read-${i}`;
+
+		is_read.style.backgroundColor = myBook.is_read === "Read" ? "#8fe58c" : "#e58c8c";
+
+		is_read.addEventListener('click', (e) => {
+			is_read.textContent = is_read.textContent === "Read" ? "Not read" : "Read";
+			is_read.style.backgroundColor = is_read.textContent === "Read" ? "#8fe58c" : "#e58c8c";
+		})
+
+		const remove = document.createElement('button');
+		remove.textContent = "Remove";
+
+		remove.addEventListener('click', (e) => {
+			myLibrary.splice(index, 1);
+			createCard()
+		})
+
+		bookCard.append(title, author, pages, is_read, remove);
+		bookGrid.appendChild(bookCard);
+	})
+}
+
+
+const modal = document.querySelector('#modal');
+const bookGrid = document.querySelector('.book-card-grid');
+const addBookBtn = document.querySelector('#add-book-btn');
+const bookForm = document.querySelector('#book-form');
+const readStatusBtn = document.querySelector('#is-read');
+
+
+addBookBtn.addEventListener('click', (e) => {
+	modal.showModal()
+})
+
+bookForm.addEventListener('submit', (e) => {
+	e.preventDefault()
+	addBookToLibrary()
+	modal.close()
+	bookForm.reset()
+})
+
+
+const theHobbit = new Book("The Hobbit", "Tolkien", 245, false);
+const theHobbit2 = new Book("The Hobbit 2", "Tolkien", 300, true);
+myLibrary.push(theHobbit);
+myLibrary.push(theHobbit2);
+createCard();
